01c8e77b0d764683416c08adba27452e79276689
Camilo Erazo <jcerazo@darumasoftware.com>
2024-09-26 17:34:38 -0500

    Correccion config para comunicacion

--- config/cluster.php
+++ config/cluster.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'endpoint' => env('API_ENDPOINT', 'http://127.0.0.1:8001/api/clustering/create-pycaret-clusters'),
+    'endpoint' => env('API_ENDPOINT', 'http://localhost:8001/api/clustering/create-pycaret-clusters'),
 ];
